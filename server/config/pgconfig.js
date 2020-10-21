8a233b02c61a4dc0fe7e3129579ae79fe83f57f7
Roni <uranoni777@gmail.com>
2020-10-22 01:47:42 +0800

    new config for version4

--- server/config/pgconfig.js
+++ server/config/pgconfig.js
@@ -1,10 +1,33 @@
+// const postgraphileOptions = {
+//   legacyRelations: "omit",
+//   watchPg: true,
+//   graphiql: true,
+//   enhanceGraphiql: true,
+//   retryOnInitFail: true,
+//   appendPlugins: [require("@graphile-contrib/pg-simplify-inflector")]
+// };
 const postgraphileOptions = {
-  legacyRelations: "omit",
+  subscriptions: true,
   watchPg: true,
+  dynamicJson: true,
+  setofFunctionsContainNulls: false,
+  ignoreRBAC: false,
+  // ignoreIndexes: false,
+  showErrorStack: "json",
+  simpleCollections: "only",
+  extendedErrors: ["hint", "detail", "errcode"],
+  appendPlugins: [require("@graphile-contrib/pg-simplify-inflector")],
+  exportGqlSchemaPath: "schema.graphql",
   graphiql: true,
   enhanceGraphiql: true,
-  retryOnInitFail: true,
-  appendPlugins: [require("@graphile-contrib/pg-simplify-inflector")]
+  allowExplain(req) {
+    // TODO: customise condition!
+    return true;
+  },
+  enableQueryBatching: true,
+  legacyRelations: "omit",
+  pgSettings(req) {
+    /* TODO */
+  },
 };
-
 module.exports = postgraphileOptions;
